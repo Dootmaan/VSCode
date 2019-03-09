d6a89dab52f4d79765707557488300f7dfedc3b3
SamWoog <njdswhy@gmail.com>
2019-03-09 10:49:55 +0800

    update bing.py

--- bing.py
+++ bing.py
@@ -1,27 +1,31 @@
 import re #导入正则表达式模块
 import requests #python HTTP客户端 编写爬虫和测试服务器经常用到的模块
-import random #随机生成一个数，范围[0,1]
 import time
 from PIL import Image
+from lxml import etree
  
 #定义函数方法
 def spiderPic(html):
-    print('正在查找壁纸,下载中，请稍后......')
-    for addr in re.findall('url: "/az/hprichbg/rb/([a-zA-Z0-9_-]*)_1920x1080.jpg"',html,re.S):     #查找URL
-        actaddr="https://cn.bing.com/az/hprichbg/rb/"+addr+"_1920x1080.jpg"
-        print('正在下载壁纸：'+addr[0:30]+'...')  #爬取的地址长度超过30时，用'...'代替后面的内容
+    print('正在查找壁纸，请稍后......')
+    html = etree.HTML(html)
+    html_data = html.xpath('/html/head/link[@id="bgLink"]/@href')   #查找URL
+    for addr in html_data:
+        actaddr="https://cn.bing.com"+addr
+        print('正在下载壁纸：'+addr[11:42]+'...')  #爬取的地址长度超过30时，用'...'代替后面的内容
  
         try:
             pics = requests.get(actaddr,timeout=10)  #请求URL时间（最大10秒）
         except requests.exceptions.ConnectionError:
             print('您当前请求的URL地址出现错误')
             continue
- 
-        fq = open('D:\\Photos\\壁纸\\' + (str(addr)+'.jpg'),'wb')     #下载图片，并保存和命名
+
+        localtime = time.localtime(time.time())
+        filename=re.findall(r'([a-zA-Z0-9_-]*)_1920x1080.jpg',addr)
+        fq = open('D:\\Photos\\壁纸\\' + (str(filename[0])+"_"+str(localtime.tm_year)+'_'+str(localtime.tm_mon)+'_'+str(localtime.tm_mday)+'.jpg'),'wb')     #下载图片，并保存和命名
         fq.write(pics.content)
         fq.close()
 
-        im = Image.open('D:\\Photos\\壁纸\\' + (str(addr)+'.jpg'))
+        im = Image.open('D:\\Photos\\壁纸\\' + (str(filename[0])+"_"+str(localtime.tm_year)+'_'+str(localtime.tm_mon)+'_'+str(localtime.tm_mday)+'.jpg'))
         im.show()
  
 #python的主方法
@@ -30,5 +34,5 @@ if __name__ == '__main__':
     result = requests.get('https://cn.bing.com/')
     # result = requests.get('https://cn.bing.com/?FORM=BEHPTB&ensearch=1')
  
-#调用函数
-spiderPic(result.text)
+    #调用函数
+    spiderPic(result.text)
